8acbd161f3d0ff3043de23474f547506a9ccfe14
Illia Khaytul <khaytul87@gmail.com>
2026-09-10 15:45:50 +0200

    feat: add user entity and persistence

---
+++ src/main/java/io/github/khaytul_illia/book_catalogue_api/user/User.java
@@ -0,0 +1,27 @@
+package io.github.khaytul_illia.book_catalogue_api.user;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "users")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+}
