ae416ee989c1b43709157d7179a4745215844b65
Illia Khaytul <khaytul87@gmail.com>
2026-09-10 16:16:35 +0200

    test: add unit tests for custom user details service and slice tests for derived user repository methods

--- src/test/java/io/github/khaytul_illia/book_catalogue_api/TestcontainersConfiguration.java
+++ src/test/java/io/github/khaytul_illia/book_catalogue_api/TestcontainersConfiguration.java
@@ -1,12 +1,15 @@
 package io.github.khaytul_illia.book_catalogue_api;
 
+import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnectionAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.testcontainers.postgresql.PostgreSQLContainer;
 import org.testcontainers.utility.DockerImageName;
 
 @TestConfiguration(proxyBeanMethods = false)
+@ImportAutoConfiguration(ServiceConnectionAutoConfiguration.class)
 public class TestcontainersConfiguration {
 
 	@Bean
