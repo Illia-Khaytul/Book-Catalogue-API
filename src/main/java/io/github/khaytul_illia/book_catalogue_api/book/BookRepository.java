212c2224dd635daf006cf6086970d28361f396c9
Illia Khaytul <khaytul87@gmail.com>
2026-09-11 10:45:26 +0200

    feat: add book entity and persistence

---
+++ src/main/java/io/github/khaytul_illia/book_catalogue_api/book/BookRepository.java
@@ -0,0 +1,10 @@
+package io.github.khaytul_illia.book_catalogue_api.book;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BookRepository extends JpaRepository<Book, Long>, JpaSpecificationExecutor<Book> {
+
+}
